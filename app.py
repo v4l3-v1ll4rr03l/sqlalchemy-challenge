0376046b069af7d8559cce9094067b1c3ce65527
v4l3-v1ll4rr03l <villarroelvalentina@MacBook-Air-6.attlocal.net>
2023-09-07 16:01:15 -0400

    finishing flask

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
+import numpy as np
 
 #################################################
 # Database Setup
@@ -23,6 +25,18 @@ station = base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
+# Helper function for queries
+def cut_off_date():
+    dates = session.query(measurement.date)
+    recent = dates[0]
+    for date in dates:
+        if recent < date:
+            recent = date
+    date = recent[0].split("-")
+    date = dt.datetime(int(date[0]), int(date[1]), int(date[2]))
+    cut_off = date - dt.timedelta(days=365)
+    return cut_off
+
 
 #################################################
 # Flask Setup
@@ -35,7 +49,6 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
 
 @app.route("/")
 def home():
@@ -50,15 +63,30 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    return "hey"
+    cut_off = cut_off_date()
+    precipitation_data = session.query(measurement.date, measurement.prcp).filter(measurement.date >= cut_off).all()
+    
+    precipitation_dict = {}
+
+    for date, precipitation in precipitation_data:
+        precipitation_dict[date] = precipitation
+
+    return jsonify(precipitation_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    return "hi"
+    station_names = session.query(station.name).all()
+    station_list= list(np.ravel(station_names))
+    return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return "hi"
+    cut_off = cut_off_date()
+    station_counts = session.query(measurement.station, func.count(measurement.prcp)).group_by(measurement.station).order_by(func.count(measurement.prcp).desc()).all()
+    most_active = station_counts[0][0]
+    temp_data = session.query(measurement.tobs).filter(measurement.station == most_active).filter(measurement.date >= cut_off).all()
+    temp_list = list(np.ravel(temp_data))
+    return jsonify(temp_list)
 
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
